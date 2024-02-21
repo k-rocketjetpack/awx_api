fe448c323183d5a0afb5271fdc6e32874dfbf595
Kali McLennan <kali@rocketjetpack.site>
2024-02-20 20:31:28 -0500

    Added GET method and parsing of inventories from the API. Also added logging utility to print colorful messages.

--- README.md
+++ README.md
@@ -6,11 +6,26 @@ There are many features of the AWX Operator that are no longer supported through
 
 ## Installation
 ### Step 1: Clone this repository
-### Step 2: Create auth.json file with contents:
+### Step 2: Create config.json file with contents:
 {
+    "protocol": "http" or "https",
+    "host": "YOUR_AWX_SERVER_IP_OR_HOSTNAME",
+    "port": "YOUR_AWX_SERVER_PORT",
+    "api_version": "YOUR_AWX_API_VERSION",
     "username": "YOUR_AWX_USERNAME_HERE",
     "password": "YOUR_AWX_PASSWORD_HERE
 }
+
+Verbose output can be enabled by adding a field named "verbose" and setting the value to true in config.json.
+
+## Implementation Status
+**Implemented**:
+Fetch list of non-smart inventories.
+Fetch list of smart inventories.
+
+**In Progress**:
+Fetch list of hosts by inventory.
+Add a host to an inventory.
 
 ## License
 GNU GPL v3
--- awx_cli.py
+++ awx_cli.py
@@ -5,12 +5,77 @@ import json
 import requests
 from requests.auth import HTTPBasicAuth
 
+# Global Variables
+DEBUG = False
+
+# Utility functions
+def LogMessage(msgType, msgText):
+    if msgType == "debug" and DEBUG == True:
+        print("[{color}DEBUG{clear}] {text}".format(color=termcolors.Cyan, clear=termcolors.End, text=msgText))
+    if msgType == "error":
+        print("[{color}DEBUG{clear}] {text}".format(color=termcolors.Red, clear=termcolors.End, text=msgText))
+    elif msgType == "warning":
+        print("[{color}DEBUG{clear}] {text}".format(color=termcolors.Yellow, clear=termcolors.End, text=msgText))
+    elif msgType == "info":
+        print("[{color}DEBUG{clear}] {text}".format(color=termcolors.Blue, clear=termcolors.End, text=msgText))
+
+class termcolors:
+    Blue = '\033[94m'
+    Cyan = '\033[96m'
+    Green = '\033[92m'
+    Yellow = '\033[93m'
+    Red = '\033[91m'
+    End = '\033[0m'
+
 class AwxAPI:
-    def __init__(self, baseUrl):
-        self._connection = AwxConnection(baseUrl)
+    def __init__(self):      
+        self.loadConfig()
+        self._connection = AwxConnection(self.Protocol, self.APIHost, self.Port, self.APIVersion, self.HTTPAuth)
+        self.GetBaseInventories()
+
+    def loadConfig(self):
+        global DEBUG
+        confFile = open("config.json")
+        confData = json.load(confFile)
+        self.HTTPAuth = HTTPBasicAuth(confData["username"], confData["password"])
+        self.APIHost = confData["host"]
+        self.Port = confData["port"]
+        self.Protocol = confData["protocol"]
+        self.APIVersion = confData["api_version"]
+        if confData["verbose"] != None and confData["verbose"]:
+            DEBUG = True
+            LogMessage("debug", "Debug messages enabled through config.json")
+            LogMessage("debug", "config.json has been parsed. Returning to AwxAPI.__init__()")
+            
+        confFile.close()
     
     def get_connection(self):
         return self._connection
+
+    def GetBaseInventories(self):
+        self.Connection.Endpoint = '/inventories'
+        inventoryData = self.Connection.Get()
+
+        if inventoryData == None:
+            LogMessage("error", "Return code of the request was not 200. No usable data was returned.".format(color=termcolors.Red, clear=termcolors.End))
+            return None
+
+        self.SmartInventories = list()
+        self.NonSmartInventories = list()
+        for result in inventoryData["results"]:
+            if result["kind"] != "smart":
+                self.NonSmartInventories.append({
+                    "name": result["name"],
+                    "id": result["id"]
+                })
+                LogMessage("debug", "Added non-smart inventory with id: {id} and name: {name}".format(id=result["id"], name=result["name"]))
+            else:
+                self.SmartInventories.append({
+                    "name": result["name"],
+                    "id": result["id"]
+                })
+                LogMessage("debug", "Added smart inventory with id: {id} and name: {name}".format(id=result["id"], name=result["name"]))
+        return True
 
     def ToString(self):
         return '''AwxAPI Object:
@@ -20,53 +85,30 @@ class AwxAPI:
     Connection = property(get_connection)
 
 class AwxConnection:
-    def __init__(self, url):
-        self._endpoint = None
-        self._baseUrl = url
-        self._password = None
-        self._username = None
+    def __init__(self, protocol, host, port, version, auth):
+        self.Protocol = protocol
+        self.APIHost = host
+        self.Port = port
+        self.APIVersion = version
+        self.HTTPAuth = auth
+        self.Headers = {
+            "Content-Type": "application/json"
+        }
+        self.BaseURL = "{protocol}://{host}:{port}/api/{version}".format(protocol=self.Protocol, host=self.APIHost, port=self.Port, version=self.APIVersion)
 
-        authFile = open("auth.json")
-        authData = json.load(authFile)
-        self.Username = authData["username"]
-        self.Password = authData["password"]
-        authFile.close()
-
-    def get_baseUrl(self):
-        return self._baseUrl
-
-    def set_baseUrl(self, value):
-        self._baseUrl = value
-
-    def get_endpoint(self):
-        return self._endpoint
-
-    def set_endpoint(self, value):
-        self._endpoint = value
-
-    def get_username(self):
-        return self._username
-
-    def set_username(self, value):
-        self._username = value
-
-    def get_password(self):
-        return self._password
-
-    def set_password(self, value):
-        self._password = value
-
-    BaseUrl = property(get_baseUrl, set_baseUrl)
-    Endpoint = property(get_endpoint, set_endpoint)
+    def Get(self):
+        LogMessage("debug", "Sending HTTP GET for {endpoint}".format(color=termcolors.Blue, clear=termcolors.End, endpoint=self.BaseURL + self.Endpoint))
+        response = requests.get( self.BaseURL + self.Endpoint, headers=self.Headers, auth=self.HTTPAuth)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
 
     def ToString(self):
         return '''    BaseUrl: {baseUrl}
-    Endpoint: {endpoint}
-    Username: {username}
-    Password: {password}'''.format(baseUrl=self._baseUrl, endpoint=self._endpoint, username=self.Username, password=self.Password)
+    Endpoint: {endpoint}'''.format(baseUrl=self.BaseURL, endpoint=self.Endpoint)
 
 if __name__ == '__main__':
-    API = AwxAPI('http://172.28.1.233:31235/api/v2')
-    API.Connection.Endpoint = '/'
-    print(API.ToString())
-
+    API = AwxAPI()
+    API.Connection.Endpoint = '/'        
+    
